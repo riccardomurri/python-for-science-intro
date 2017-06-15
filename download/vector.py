509e16abde14585317611654dd775bcb2d2023ae
Riccardo Murri <riccardo.murri@gmail.com>
2017-06-15 23:00:28 +0200

    Add exercise sheet for part 09.

---
+++ download/vector.py
@@ -0,0 +1,12 @@
+class Vector(object):
+  """A 2D Vector."""
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+  def add(self, other):
+    return Vector(self.x+other.x,
+                  self.y+other.y)
+  def mul(self, scalar):
+    return Vector(scalar*self.x, scalar*self.y)
+  def show(self):
+    return ("<%g,%g>" % (self.x, self.y))
