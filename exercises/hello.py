bbe4e3b49d9b934e1307f6036fb005b04d069977
Riccardo Murri <riccardo.murri@gmail.com>
2017-06-15 23:26:28 +0200

    Add exercise sheet files.
    They are the same as solution notebooks, except that actual solutions have been
    removed.

---
+++ exercises/hello.py
@@ -0,0 +1,16 @@
+#
+# Sample Python module
+#
+
+# A Python module is just a file containing Python commands.
+# Although *any* Python statement is permitted in a module file,
+# only constants, functions, and class definitions should
+# normally appear in a Python module file.
+
+def greet(name):
+  """A friendly function."""
+  print("Hello, " + name + "!")
+  
+# the following is just for demo purposes during the course,
+# it should not appear in a real module (see above).
+greet("world")
